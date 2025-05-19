b4f008b47893ebaed135e227951115309b7c9bef
isabella trochez <isabellatrochez@unicauca.edu.co>
2025-05-19 17:39:02 -0500

    Agregando archivos del tercer corte

--- student/src/main/java/com/example/student/Config/RabbitMQStudentConfig.java
+++ student/src/main/java/com/example/student/Config/RabbitMQStudentConfig.java
@@ -1,6 +1,9 @@
-package com.example.student.Config;
+package com.example.student;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
